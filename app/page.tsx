acb58417ad197ff2793e6b43c3c141e69c575ee6
岡本柊 <okamotoshu@okamotoshuunoMacBook-Air.local>
2023-11-29 15:13:16 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
   // const cookie = await getUidFromCookie();
   // console.log("cookie: ", cookie);
   console.log("okamoto");
+  console.log("okamoto");
   const router = useRouter();
 
   useEffect(() => {
