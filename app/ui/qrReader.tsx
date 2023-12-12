97e02f96ce534b9e056ccea6e6653223e44e23d9
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2023-12-12 17:52:02 +0900

    QRコードリーダー実装

---
+++ app/ui/qrReader.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useZxing } from "react-zxing";
+import { useRouter } from "next/navigation";
+
+export default function BarcodeScanner() {
+  const router = useRouter();
+  const [result, setResult] = useState("");
+  const { ref } = useZxing({
+    onDecodeResult(result) {
+      setResult(result.getText());
+    },
+  });
+
+  useEffect(() => {
+    router.push(result);
+  }, [result]);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-0 text-center">
+      <div className="justify-center mt-24">
+        <h1 className="text-2xl font-bold mb-4">QRコードリーダー</h1>
+        <video ref={ref} className="m-auto" />
+        <p className="pt-5 pl-5 pr-5">QRコードを読み取ってください</p>
+        <p className="pt-5 pl-5 pr-5">
+          カメラが起動しない場合は、ブラウザの設定からカメラを許可してください。
+        </p>
+      </div>
+    </main>
+  );
+}
