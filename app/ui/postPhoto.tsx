f017359f656002ef5d458b71c133cea04ec8e6cf
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2023-12-19 16:19:48 +0900

    postphotoの投稿ボタンを1回しか押せないように修正

--- app/ui/postPhoto.tsx
+++ app/ui/postPhoto.tsx
@@ -16,6 +16,7 @@ export default function UploadImage() {
   const [photo, setPhoto] = useState<File | null>(null);
   const [error, setError] = useState("");
   const [createObjectURL, setCreateObjectURL] = useState("");
+  const [isPushButton, setIsPushButton] = useState(false);
 
   const uploadToClient = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
@@ -73,12 +74,21 @@ export default function UploadImage() {
       <div className="justify-center mt-24">
         <div className="flex justify-between items-center w-full p-5">
           <h1 className="text-left text-2xl">新規投稿</h1>
-          <button
-            onClick={() => uploadToServer()}
-            className="flex justify-center items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            投稿
-          </button>
+          {isPushButton ? (
+            <button className="flex justify-center items-center bg-gray-600 text-white font-bold py-2 px-4 rounded">
+              投稿
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                setIsPushButton(true);
+                uploadToServer();
+              }}
+              className="flex justify-center items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              投稿
+            </button>
+          )}
         </div>
         <div className="text-black">
           {createObjectURL && (
