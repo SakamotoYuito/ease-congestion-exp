571272707aa6e768cc7e0268fecb6b44abcb2c48
坂本唯斗 <sakamotoyuito@sakamotoyuitonoMacBook-Air.local>
2024-01-02 03:42:36 +0900

    マニフェスト編集, budouxをクライアントのみで動くように修正

--- next.config.js
+++ next.config.js
@@ -4,7 +4,20 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  runtimeCaching: [],
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/ease-congestion-exp.vercel.app\//,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "offlineCache",
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 24 * 60 * 60,
+        },
+        networkTimeoutSeconds: 10,
+      },
+    },
+  ],
   disable: process.env.NODE_ENV === "development",
 });
 
