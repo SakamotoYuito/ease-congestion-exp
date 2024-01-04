b13df659de895bfc6f57f9725c62ece6e74854fc
Sho Mitarai <shomitarai1@gmail.com>
2024-01-04 16:37:53 +0900

    通知機能を実装

--- app/notification/page.tsx
+++ app/notification/page.tsx
@@ -1,11 +1,13 @@
 import HeaderComponent from "../ui/header";
 import FooterComponent from "../ui/footer";
+import { NotificationComponent } from "../ui/notification";
 
 export default function Notification() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0 text-center">
       <HeaderComponent />
       <h1 className="text-2xl text-center mt-24">Coming soon...</h1>
+      <NotificationComponent />
       <FooterComponent />
     </main>
   );
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,22 +1,49 @@
-// importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
-// importScripts(
-//   "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
-// );
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+);
 
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_APIKEY,
-//   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_PROJECTID,
-//   storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-//   appId: process.env.NEXT_PUBLIC_APPID,
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBuM-PSRIXKeCDRruZ9dX7h7V6rQLXAW3A",
+  authDomain: "ksu-solve-congestion.firebaseapp.com",
+  projectId: "ksu-solve-congestion",
+  storageBucket: "ksu-solve-congestion.appspot.com",
+  messagingSenderId: 1026118046498,
+  appId: "1:1026118046498:web:400e71c179f7e1186ecb7b",
+};
 
-// const messaging = firebase.messaging();
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
 
-// messaging.onBackgroundMessage((payload) => {
-//   console.log(
-//     "[firebase-messaging-sw.js] Received background message ",
-//     payload
-//   );
-// });
+// 通知を受けとると push イベントが呼び出される。
+self.addEventListener(
+  "push",
+  function (event) {
+    let message = event.data.json();
+    console.log("event:push", message);
+    let messageTitle = message.notification.title;
+    let messageBody = message.notification.body;
+    let tag = "cuppa";
+
+    const notificationPromise = self.registration.showNotification(
+      messageTitle,
+      {
+        icon: "/img/icons/favicon-32x32.png",
+        body: messageBody,
+        tag: tag,
+      }
+    );
+
+    event.waitUntil(notificationPromise);
+  },
+  false
+);
+
+// WEBアプリがバックグラウンドの場合にはsetBackGroundMessageHandlerが呼び出される。
+messaging.setBackgroundMessageHandler(function (payload) {
+  console.log("backgroundMessage");
+
+  return self.registration.showNotification(payload.title, {
+    body: payload.body,
+  });
+});
