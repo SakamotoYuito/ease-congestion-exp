631d5cf63e70412a685b2f0d387b5dda1533eeca
Sho Mitarai <shomitarai1@gmail.com>
2024-01-06 22:15:52 +0900

    未読の通知がある場合に通知アイコンの色を赤色に変更する機能を実装

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -8,9 +8,12 @@ import {
   faQrcode,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { NotificationComponent } from "./notification";
+import { getUserFromCookie } from "@/lib/session";
+import { fetchNotificationInfo } from "@/lib/dbActions";
 
 export default function FooterComponent() {
   const currentPath = usePathname();
@@ -21,6 +24,25 @@ export default function FooterComponent() {
 
   const selectedIndex = paths.indexOf(currentPath);
   const [selectedIcon, setSelectedIcon] = useState(selectedIndex);
+
+  const [isReadAllNotification, setIsReadAllNotification] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const user = await getUserFromCookie();
+      if (!user) return;
+      const uid = user.uid;
+      const notificationList = await fetchNotificationInfo();
+
+      notificationList.forEach((notification: any) => {
+        // 未読の通知が存在すればfalseに設定
+        if (!notification.readUser.includes(uid)) {
+          console.log("set false!");
+          setIsReadAllNotification(false);
+        }
+      });
+    })();
+  }, []);
 
   return (
     <div className="fixed bottom-0 w-full border-t border-gray-300 z-10 bg-[#f5ffec] h-20">
@@ -33,23 +55,41 @@ export default function FooterComponent() {
                 setSelectedIcon(index);
               }}
             >
-              <FontAwesomeIcon
-                icon={icon}
-                style={{
-                  width: "25px",
-                  height: "25px",
-                  color: selectedIcon === index ? "green" : "black",
-                }}
-              />
-              <div
-                className={`text-xs mb-2 ${
-                  selectedIcon === index
-                    ? "text-green-700 font-bold"
-                    : "text-black"
-                }`}
-              >
-                {title[index]}
-              </div>
+            {icon===faBell ? (
+              < >
+                {isReadAllNotification ? (
+                  <FontAwesomeIcon
+                    icon={icon}
+                    style={{
+                      width: "25px",
+                      height: "25px",
+                      color: selectedIcon === index ? "green" : "black",
+                    }}
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    icon={icon}
+                    style={{
+                      width: "25px",
+                      height: "25px",
+                      color: selectedIcon === index ? "green" : "red",
+                    }}
+                  />
+                )}
+              </>
+            ):(
+                <FontAwesomeIcon
+                  icon={icon}
+                  style={{
+                    width: "25px",
+                    height: "25px",
+                    color: selectedIcon === index ? "green" : "black",
+                  }}
+                />
+            )}
+            <div className={`text-xs mb-2 ${selectedIcon === index ? "text-green-700 font-bold" : "text-black"}`}>
+              {title[index]}
+            </div>
             </button>
           </Link>
         ))}
